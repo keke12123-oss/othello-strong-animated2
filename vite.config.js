e212377cf54c20bbb51316dcbb12957c367b0b79
keke12123-oss <keke12123@gmail.com>
2025-08-09 15:34:16 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: true, // 0.0.0.0 で待ち受け
     port: 5173,
     strictPort: true,
-    allowedHosts: ["*"],
-    hmr: { clientPort: 443 }
+    allowedHosts: [
+      "h79rln-5173.csb.app", // CodeSandboxのあなたの環境ドメイン
+      "localhost"
+    ],
+    hmr: {
+      clientPort: 443 // HMRを443経由で
+    }
   }
 });
