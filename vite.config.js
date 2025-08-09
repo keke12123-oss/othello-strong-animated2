5f10ba1e8a0f4f3944dea7543ecdc1de8157753f
keke12123-oss <keke12123@gmail.com>
2025-08-09 15:26:55 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["*"]
+    host: true,
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ["*"],
+    hmr: { clientPort: 443 }
   }
 });
